c0853e16882b61b340a346927f415bc0c11607f9
Kevin Zeng <zengfenfei@gmail.com>
2016-08-12 11:03:12 +0800

    Fix test

--- src/test/auth.ts
+++ src/test/auth.ts
@@ -8,8 +8,8 @@ let ringcentral;
 let p = fetch(testConfig.authDataUrl).then(function (res) {
     return res.json();
 }).then(function (auth) {
-    ringcentral = new Ringcentral(auth.app).platform();
-    return ringcentral.login(auth.user);
+    ringcentral = new Ringcentral(auth.app);
+    return ringcentral.platform().login(auth.user);
 }).then(function (res) {
     return ringcentral;
 });
