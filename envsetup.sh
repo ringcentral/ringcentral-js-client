4f33631e729b2aead745073bfdba62be89b1e077
Kevin Zeng <zengfenfei@gmail.com>
2016-08-16 14:40:03 +0800

    Add build to travis

--- envsetup.sh
+++ envsetup.sh
@@ -4,5 +4,11 @@ if [ $? != '0' ]; then
 echo Fail to setupenv. This script should be executed in the project root directory.
 return
 fi;
-export PATH=`pwd`:$PATH
-cd $original_dir
+
+local_bin=`pwd`;
+cd $original_dir;
+
+if [[ $PATH != *$local_bin* ]]
+then
+  export PATH=$local_bin:$PATH;
+fi
---
+++ src/typings/ringcentral/ringcentral.d.ts
@@ -0,0 +1,10 @@
+declare module "ringcentral" {
+	
+    class Ringcentral {
+        constructor(config);
+        platform();
+    }
+    
+	namespace Ringcentral {}
+	export = Ringcentral;
+}
