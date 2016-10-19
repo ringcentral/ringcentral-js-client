c27e91d0b909ef2e05c5bd4521a200ef07f517e6
Kevin Zeng <zengfenfei@gmail.com>
2016-10-19 16:14:34 +0800

    Move code generator  to a seperate project

--- codegen/plugins/list-response.js
+++
@@ -1,9 +0,0 @@
-/**
- * Test if response is a list, then use PagingResult as model
- */
-module.exports = function (schema) {
-    var props = schema.properties;
-    if (schema.type == "object" && props && props.records && props.navigation && props.paging) {
-        return "PageResult";
-    }
-}
